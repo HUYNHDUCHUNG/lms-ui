62e6d9acf7ea5a574cdfbf9f5916fce8b7405a9e
Hung <huynhhung2108@gmail.com>
2024-06-14 13:11:40 +0700

    fix: Custom Color Title

--- app/[locale]/(landing-page)/_components/intructors.tsx
+++ app/[locale]/(landing-page)/_components/intructors.tsx
@@ -15,9 +15,9 @@ const Intructors = () => {
   return (
     <div className='px-32 pt-8'>
       <div className='flex justify-center'>
-        <h1 className='text-3xl font-bold'>Intructors</h1>
+        <h1 className='text-3xl font-bold drop-shadow-custom bg-gradient-to-r from-[#a8c0ff] to-[#3f2b96] bg-clip-text text-transparent'>Intructors</h1>
       </div>
-      <div >
+      <div className="pt-4">
         <Slider {...settings}>
         <IntructorItem />
         <IntructorItem />
--- app/[locale]/(landing-page)/_components/title.tsx
+++ app/[locale]/(landing-page)/_components/title.tsx
@@ -3,9 +3,9 @@ import { ImCompass } from "react-icons/im";
 const Title = () => {
 
     return (
-        <div className="mt-16 pt-16 px-32">
+        <div className="mt-16 pt-16 px-32 ">
             <div className="flex justify-center items-center">
-                <h1 className="text-3xl font-bold">FAT1000 Program - Future AI Talents</h1>
+                <h1 className="text-3xl font-bold drop-shadow-custom bg-gradient-to-r from-[#a8c0ff] to-[#3f2b96] bg-clip-text text-transparent">FAT1000 Program - Future AI Talents</h1>
             </div>
             <div className="flex justify-center gap-2 pt-6">
                 <ImCompass size={32} />
